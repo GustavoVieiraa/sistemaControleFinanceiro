0a8129ef591518c34e7fc0c9830a51652472c333
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-05-18 11:28:39 -0300

    criação da base do projeto
    Criação base, da descrição do projeto, index, styles e img.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../sistemaControleFinanceiro/styles/style.css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <title>myFinances</title>
+</head>
+<body>
+    <div class="menu-login">
+        <div class="esquerda-login">
+            <h1>Registre-se<br> E controle suas finanças de um jeito prático e eficaz.</h1>
+        </div>
+        <div class="direita-login">
+            <form action=""></form>
+        </div>
+    </div>
+</body>
+</html>
